09ef4fc23e5cd72cd672443af8cf290543e9883a
mohamedalaa1197 <mohamed.mohdy1197@gmail.com>
2022-05-10 19:04:09 +0200

    Migrations

---
+++ Voucher_System/Migrations/20220510113517_generateDBAndTables.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Voucher_System.Migrations
+{
+    public partial class generateDBAndTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Points = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Merchent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Merchent", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voucher",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VoucherType = table.Column<int>(type: "int", nullable: false),
+                    NoOfUsage = table.Column<int>(type: "int", nullable: false),
+                    VoucherCategory = table.Column<int>(type: "int", nullable: false),
+                    MerchentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voucher", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Voucher_Merchent_MerchentId",
+                        column: x => x.MerchentId,
+                        principalTable: "Merchent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerVoucher",
+                columns: table => new
+                {
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VoucherId = table.Column<int>(type: "int", nullable: false),
+                    VoucherId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerVoucher", x => new { x.CustomerId, x.VoucherId });
+                    table.ForeignKey(
+                        name: "FK_CustomerVoucher_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CustomerVoucher_Voucher_VoucherId1",
+                        column: x => x.VoucherId1,
+                        principalTable: "Voucher",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerVoucher_VoucherId1",
+                table: "CustomerVoucher",
+                column: "VoucherId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voucher_MerchentId",
+                table: "Voucher",
+                column: "MerchentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerVoucher");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Voucher");
+
+            migrationBuilder.DropTable(
+                name: "Merchent");
+        }
+    }
+}
